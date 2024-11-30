22ef9e53210bb649f1fd93703a3012fef2eb1f2b
timolor <timolordave@gmail.com>
2024-11-30 21:36:03 +0100

    reduced user data

--- src/models/user.ts
+++ src/models/user.ts
@@ -11,21 +11,10 @@ export interface IUser {
 	phone: string;
 	password: string;
 	occupation: string;
-	campus: string;
-	marital_status: string;
-	device_token: string[];
 	referred_by: string;
-	roles: string[];
-	permissions: string[];
-	activation_code: string;
-	activation_expires_in: Date;
 	isActive: boolean;
-	isSuspended: boolean;
-	isDeleted: boolean;
 	isFaceIndexed: boolean;
 	online: boolean;
-	bornAgain: boolean;
-	notification_counter: number;
 	status: boolean;
 	profile_picture: string;
 	location: {
@@ -34,8 +23,6 @@ export interface IUser {
 		state: string;
 		country: string;
 	};
-	cell_id: string;
-	firebase_uid: string;
 	readonly createdAt: Date;
 	updatedAt: Date;
 	//faceId?: string; // Optional field for Rekognition FaceId
